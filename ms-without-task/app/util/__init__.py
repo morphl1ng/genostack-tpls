e47cadde0b9deb61973380e4cfe06efb8a6bbdb2
ice-melt <ice-melt@outlook.com>
2023-10-16 18:11:27 +0800

    clean project

--- ms-without-task/app/util/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-"""
-@Author     : ice-melt@outlook.com
-@File       : __init__.py.py
-@Time       : 2023/08/17
-@Version    : 1.0
-@Desc       : None
-"""
